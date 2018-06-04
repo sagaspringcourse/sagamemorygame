2a9758821807569f3aab067aa5a16e18601a1fd6
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-06-04 14:41:14 +0200

    memgame client

---
+++ memorygame/src/main/java/rs/saga/memorygame/memorygame/MemorygameApplication.java
@@ -0,0 +1,12 @@
+package rs.saga.memorygame.memorygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorygameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorygameApplication.class, args);
+    }
+}
